b198e04180ccffb1936b081b4eb805ed70375ac1
Joe Gesualdo <joegesualdo@gmail.com>
2016-03-26 09:08:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,136 @@
+"use strict";
+
+var csi = require("control-sequence-introducer");
+
+function moveTerminalCursor(type, opts) {
+  opts = opts || {};
+
+  switch(type) {
+    case "up":
+      cursorUp(opts.count || opts.row || 1)
+      break;
+    case "down":
+      cursorDown(opts.count || opts.row || 1)
+      break;
+    case "right":
+      cursorForward(opts.count || opts.col || 1)
+      break;
+    case "left":
+      cursorBack(opts.count || opts.col || 1)
+      break;
+    case "nextLine":
+      cursorNextLine(opts.count || 1)
+      break;
+    case "prevLine":
+      cursorPrevLine(opts.count || 1)
+      break;
+    case "toCol":
+      if (!opts.col) {
+        throw new Error("Must provide 'col' option for the 'toCol' movement type.");
+      }
+      cursorToColumn(opts.col || 1)
+      break;
+    case "toPos":
+      if (!opts.row || !opts.col) {
+        throw new Error("Must provide 'row' and 'col' options for the 'toPos' movement type.");
+      }
+      cursorToPosition(opts.col, opts.row)
+      break;
+    default:
+      throw new Error("Must provide a valid type argument.");
+      break;
+  }
+}
+
+// moveTerminalCursor("toPos", {row: 10, col: 10})
+// moveTerminalCursor("right", {count: 10})
+// moveTerminalCursor("left", {count: 5})
+moveTerminalCursor("upp", {count: 5})
+moveTerminalCursor("toCol", {col: 10})
+moveTerminalCursor("toPos", {row: 10, col: 10})
+console.log("Meow")
+
+// Helper Cursor Functions ===================================================
+// TODO: Extract String functions into seprate modules.
+// Moves the cursor cellNum (default 1) cells in the given direction. If the cursor is already 
+//   at the edge of the screen, this has no effect.
+function cursorUpString(cellNum) {
+  return csi + cellNum + "A"
+}
+function cursorUp(cellNum) {
+  process.stdout.write(cursorUpString(cellNum))
+}
+
+// Moves the cursor cellNum (default 1) cells in the given direction. If the cursor is already 
+//   at the edge of the screen, this has no effect.
+function cursorDownString(cellNum) {
+  return csi + cellNum + "B"
+}
+function cursorDown(cellNum) {
+  process.stdout.write(cursorDownString(cellNum))
+}
+
+// Moves the cursor cellNum (default 1) cells in the given direction. If the cursor is already 
+//   at the edge of the screen, this has no effect.
+function cursorForwardString(cellNum) {
+  return csi + cellNum + "C"
+}
+function cursorForward(cellNum) {
+  process.stdout.write(cursorForwardString(cellNum))
+}
+
+// Moves the cursor cellNum (default 1) cells in the given direction. If the cursor is already 
+//   at the edge of the screen, this has no effect.
+function cursorBackString(cellNum) {
+  return csi + cellNum + "D"
+}
+function cursorBack(cellNum) {
+  process.stdout.write(cursorBackString(cellNum))
+}
+
+
+// Moves cursor to beginning of the line n (default 1) lines down. (not ANSI.SYS)
+function cursorNextLineString(n) {
+  return csi + n + "E"
+}
+function cursorNextLine(n) {
+  process.stdout.write(cursorNextLineString(n))
+}
+// Moves cursor to beginning of the line n (default 1) lines up. (not ANSI.SYS)
+function cursorPrevLineString(n) {
+  return csi + n + "F"
+}
+function cursorPrevLine(n) {
+  process.stdout.write(cursorPrevLineString(n))
+}
+
+// Moves the cursor to column n (default 1). (not ANSI.SYS)
+function cursorToColumnString(n) {
+  return csi + n + "G"
+}
+function cursorToColumn(n) {
+  process.stdout.write(cursorToColumnString(n))
+}
+
+// Moves the cursor to row n, column m. The values are 1-based, and default to 1 (top left corner) if omitted.
+function cursorToPositionString(row, col) {
+  return csi + row + ";" + col + "H"
+}
+function cursorToPosition(row, col) {
+  process.stdout.write(cursorToPositionString(row, col))
+}
+
+function hideCursorString() {
+  return csi + "?25l"
+}
+function hideCursor() {
+  process.stdout.write(hideCursorString())
+}
+function showCursorString() {
+  return csi + "?25h"
+}
+function showCursor() {
+  process.stdout.write(showCursorString())
+}
+
+module.exports = moveTerminalCursor;
---
+++ readme.md
@@ -0,0 +1,107 @@
+## Move Terminal Cursor
+> Move a terminal cursor (right, left, up, dow, etc).
+
+## Install
+```
+$ npm install --save move-terminal-cursor 
+```
+
+## Usage
+```javascript
+var move = require("move-terminal-cursor")
+
+move("up", {count: 5})
+move("down", {count: 5})
+move("right", {count: 5})
+move("left", {count: 5})
+move("nextLine", {count: 5})
+move("prevLine", {count: 5})
+move("toCol", {col: 10})
+move("toPos", {row: 10, col: 10})
+
+console.log("Meow")
+```
+
+## API
+### `move(type, opts)`
+> Moves the terminal cursor.
+
+| Name | Type | Description |
+|------|------|-------------|
+| type | `String` | Where you want the cursor to move.
+| opts | `Object` | Options (see available options below)
+
+#### Available types 
+|types |
+|------|
+| [up](#up)   |
+| [down](#down) |
+| [right](#right)|
+| [left](#left) |
+| [nextLine](#nextLine) |
+| [prevLine](#prevLine) |
+| [toCol](#toCol) |
+| [toPos](#toPos) |
+
+#### Available options 
+| Name | Type | Description |
+|------|------|-------------|
+| x | `Number` | The x cord.
+| y | `Number` | The y cord.
+| count | `Number` | The number of movements.
+
+## Required options for movement types.
+### "up"<a name="up"></a>
+### `move("up", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("up", {count: 5})
+```
+### "down"<a name="down"></a>
+### `move("down", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("down", {count: 5})
+```
+### "right"<a name="right"></a>
+### `move("right", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("right", {count: 5})
+```
+### "left"<a name="left"></a>
+### `left("right", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("left", {count: 5})
+```
+### "nextLine"<a name="nextLine"></a>
+### `move("nextLine", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("nextLine", {count: 5})
+```
+### "prevLine"<a name="prevLine"></a>
+### `move("prevLine", opts)`
+> Must provide 'count' key/value to options object.
+```javascript
+move("prevLine", {count: 5})
+```
+### "toCol"<a name="toCol"></a>
+### `move("toCol", opts)`
+> Must provide 'col' key/value to options object.
+```javascript
+move("toCol", {col: 5})
+```
+### "toPos"<a name="toPos"></a>
+### `move("toPos", opts)`
+> Must provide 'col' and 'row' key/values to options object.
+```javascript
+move("toPos", {row: 5, col: 10})
+```
+
+## Related
+- [terminal-cursor](https://github.com/joegesualdo/terminal-cursor) - Manipulate and keep track of the terminal cursor (move, hide, show, etc)
+
+## License
+MIT © [Joe Gesualdo]()
